d64ee4c31ea775fd1dbe8a4fa2c5fbf0868d9839
guelting <shwang1@andrew.cmu.edu>
2021-09-16 20:24:36 -0400

    initial commit
    Adding necessary files

---
+++ hw8.py
@@ -0,0 +1,18 @@
+import cs112_s21_week8_linter
+import math, copy, random
+
+from cmu_112_graphics import *
+
+def playTetris():
+    print('Replace this with your Tetris game!')
+
+#################################################
+# main
+#################################################
+
+def main():
+    cs112_s21_week8_linter.lint()
+    playTetris()
+
+if __name__ == '__main__':
+    main()
